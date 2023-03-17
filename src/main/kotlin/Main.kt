6634799e54de89b4e2fcd2642f36727804f50cd1
Andrei Prokudin <andrewicehab@gmail.com>
2023-03-17 12:48:15 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val likes: Int = 698701
+    var prefix: String = if (likes == 1) {
+        "человеку"
+    } else if ((likes - 1) % 10 == 0) {
+        "человеку"
+    } else {
+        "людям"
+    }
+
+    if (likes > 0) print("понравилось $likes $prefix")
+}
