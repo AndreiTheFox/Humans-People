59f7b9d7a5cd42d0b19fdb7a78e56de778b2bd49
Andrei Prokudin <andrewicehab@gmail.com>
2023-03-17 21:34:51 +0700

    Add condition for *11 numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 fun main() {
-    val likes: Int = 698701
+    val likes: Int = 511
     val prefix: String = if (likes == 1) {
         "человеку"
-    } else if ((likes - 1) % 10 == 0) {
+    } else if (((likes + 89)) %100 == 0) {
+        "людям"
+    }
+    else if ((likes - 1) % 10 == 0) {
         "человеку"
     } else {
         "людям"
